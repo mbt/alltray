730c4d16490d4457e6dbc5b5308b23867481b3da
Michael B. Trausch <mike@trausch.us>
2009-06-09 15:49:12 -0400

    GRR.

---
+++ src/gtop_glue.h
@@ -0,0 +1,12 @@
+/*
+ * gtop_glue.h - Use the libgtop library, but I am too lazy/annoyed to try
+ * to figure out how the ^#!! I'm supposed to bind it to Vala.
+ * Copyright (C) 2009 Michael B. Trausch <mike@trausch.us>
+ * License: GNU LGPL 3.0 as published by the Free Software Foundation.
+ */
+#ifndef __gtop_glue_h_included__
+#define __gtop_glue_h_included__
+
+int alltray_get_ppid_for(int pid);
+
+#endif /* __gtop_glue_h_included__ */
---
+++ vapi/gtop_glue.vapi
@@ -0,0 +1,5 @@
+[CCode(cheader_filename = "gtop_glue.h")]
+namespace AllTray {
+	[CCode(cname = "alltray_get_ppid_for")]
+	public int get_ppid_for(int pid);
+}
