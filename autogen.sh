1dae6f817f551ec9872eaa57f49adbded2313849
Michael B. Trausch <mike@trausch.us>
2009-09-29 15:40:29 -0400

    Add flag to tell autogen.sh to not run ./configure when not desired

--- autogen.sh
+++ autogen.sh
@@ -3,9 +3,13 @@
 # Generate the build system for AllTray.
 #
 
-printf "I will run ./configure after generating it and pass it all options\n"
-printf "you pass to me.  Send me a SIGINT and re-run me if you want to pass\n"
-printf "any configure options.\n\n"
+if [ "$1" == "--no-configure" ]; then
+    autoreconf --force --install
+else
+    printf "I will run ./configure after generating it and pass it all options\n"
+    printf "you pass to me.  Send me a SIGINT and re-run me if you want to pass\n"
+    printf "any configure options.\n\n"
 
-autoreconf --force --install
-./configure "$@"
+    autoreconf --force --install
+    ./configure "$@"
+fi
